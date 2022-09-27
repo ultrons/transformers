fb57b5568c14d65e9609e19fc731e1752d6eea95
Alex Wertheim <awertheim@google.com>
2022-09-27 17:34:58 +0000

    Updated multiprocessing function in xla_spawn.py

--- examples/pytorch/xla_spawn.py
+++ examples/pytorch/xla_spawn.py
@@ -77,7 +77,7 @@ def main():
     # Patch sys.argv
     sys.argv = [args.training_script] + args.training_script_args + ["--tpu_num_cores", str(args.num_cores)]
 
-    pjrt.run_multiprocess(mod._mp_fn)
+    pjrt._run_multiprocess(mod._mp_fn)
     # xmp.spawn(mod._mp_fn, args=(), nprocs=args.num_cores)
 
 
